3dbff0f047fd67a1dd0f170d9387ff0cacdf9bfe
Vadim <vadimshut1992@gmail.com>
2020-11-03 14:39:48 -0500

    Simple arp network scanner

--- main.py
+++ main.py
@@ -1,48 +1,47 @@
 #!/usr/bin/env python
 import scapy.all as scapy
-import optparse
+import argparse
+from colorama import init, Fore, Back, Style
 
 
 def get_arguments():
-    parser = optparse.OptionParser()
-    parser.add_option("-t", "--target", dest="ip_range", help="IP range for scanning.Example: 192.168.1.1/24")
-    options, arguments = parser.parse_args()
-    if not options.ip_range:
-        parser.error("[-] Please specify the IP range, use --help for more info.")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--target", dest="target", help="IP range / IP for scanning.Example: 192.168.1.1/24")
+    options = parser.parse_args()
+    if not options.target:
+        parser.error(Fore.RED + "[-]" + Fore.GREEN + "Please specify the IP range, use --help for more info.")
     # elif not checking_ip_range_format(options.ip_range):
     #     parser.error("[-] IP range entered incorrectly, use --help for more info.")
-    print(options)
-    return options
+    return options.target
 
 
-def checking_ip_range_format(ip_range):
+def checking_ip_range_format():
     """FIX THIS FUNCTION!!!"""
     pass
 
 
+def print_result(func):
+    def wrapper(*args, **kwargs):
+        print(
+            Fore.YELLOW + f"{'-' * 33}\nIP\t\tAt MAC Address\n{'-' * 33}")
+        result_list = func(*args, **kwargs)
+        for client in result_list:
+            print(f"{client['ip']}\t{client['mac']}")
+        print(Fore.YELLOW + f"{'-' * 33}\n")
+    return wrapper
+
+
+@print_result
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
-    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    arp_request_broadcast = broadcast / arp_request
+    ether_broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = ether_broadcast / arp_request
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
-    clients_list = []
-    for element in answered_list:
-        client_dict = {"ip": element[1].psrc,
-                       "mac": element[1].hwsrc}
-
-        clients_list.append(client_dict)
+    clients_list = [{"ip": element[1].psrc, "mac": element[1].hwsrc} for element in answered_list]
     return clients_list
 
 
-def print_result(result_lists):
-    print("IP\t\t\tAt MAC Address\n-----------------------------")
-    for client in result_lists:
-        print(f"{client['ip']}\t{client['mac']}")
-
-
 if __name__ == "__main__":
-    # print_result(scan(get_arguments()))
-    ip = get_arguments()
-    print(ip)
-    scan_result = scan(ip)
-    print_result(scan_result)
+    init(autoreset=True)
+    scan(get_arguments())
+
